b4fa61f8b648dff12a32b853655844c4c5f907df
Stepan <Stepantryaschin86@gmail.com>
2023-07-10 19:45:15 +0400

    parser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,23 +16,11 @@ import java.util.List;
 
 public class Main {
 
-<<<<<<< HEAD
+
     public static void main(String[] args) {
         Parser parser = new Parser();
-       VisibilityDB visibilityDB = new VisibilityDB();
-        visibilityDB.completion("Ulan-Ude.txt",3);
-     /*   for (int i = 0; i < parser.parse("Moscow.txt").size()/2; i++) {
+     for (int i = 0; i < parser.parse("Moscow.txt").size()/2; i++) {
             System.out.println(i +" " + parser.getName().get(i)+ " " + parser.getDateStart().get(i)+ " " +parser.getDateEnd().get(i));
-        }*/
-=======
-    public static void main(String[] args) throws ParseException {
-
-        TimeInterval timeInterval = new TimeInterval(
-                new Time("2023-07-01 00:13:35"),
-                new Time("2023-07-01 00:15:15")
-        );
-        System.out.println(timeInterval.getTimeIntervalSecond());
->>>>>>> e1475f27f5a354e8d2566111074f09e47837056b
-
+        }
     }
 }
